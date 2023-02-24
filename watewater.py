6c84ee97d1de55cd604305edaba02f9009304a30
benrafalski <79751177+benrafalski@users.noreply.github.com>
2023-02-24 15:01:27 -0700

    numbers are good

--- watewater.py
+++ watewater.py
@@ -1,3 +1,4 @@
+import time
 import pandas as pd 
 import tenseal as ts
 import numpy as np
@@ -67,8 +68,8 @@ CONST2 = np.array([0.15]*7)
 
 
 def context():
-    poly_mod_degree = 8192
-    coeff_mod_bit_sizes = [60, 40, 40, 60]		
+    poly_mod_degree = 8192*2
+    coeff_mod_bit_sizes = [60, 40, 40, 40, 60]		
     ctx = ts.context(ts.SCHEME_TYPE.CKKS, poly_mod_degree, -1, coeff_mod_bit_sizes)
     ctx.global_scale = pow(2, 40)
     ctx.generate_galois_keys()
@@ -92,26 +93,26 @@ ctx = context()
 # Area 3: [(C_T3*D_T3) - [A_M2*(B_M2*0.85)]] - (E-F)
 
 
-C_T1 = np.array([42.28])
-D_T1 = np.array([58376509.76])
-A_M1 = np.array([70.94])
-B_M1 = np.array([17810352.47])
-A_M2 = np.array([56.78])
-B_M2 = np.array([43427157.25])
-C_DT = np.array([54.79])
-D_DT = np.array([3355764.17])
-C_CR = np.array([60.76])
-D_CR = np.array([4180563.65])
-C_T2 = np.array([26.80])
-D_T2 = np.array([17539221.87])
-C_S = np.array([16.78])
-D_S = np.array([2742756.14])
-C_T3 = np.array([50.29])
-D_T3 = np.array([50824570.13])
-E = np.array([62.08])
-F = np.array([946350.00])
-CONST1 = np.array([0.85])
-CONST2 = np.array([0.15])
+# C_T1 = np.array([42.28])
+# D_T1 = np.array([58376509.76])
+# A_M1 = np.array([70.94])
+# B_M1 = np.array([17810352.47])
+# A_M2 = np.array([56.78])
+# B_M2 = np.array([43427157.25])
+# C_DT = np.array([54.79])
+# D_DT = np.array([3355764.17])
+# C_CR = np.array([60.76])
+# D_CR = np.array([4180563.65])
+# C_T2 = np.array([26.80])
+# D_T2 = np.array([17539221.87])
+# C_S = np.array([16.78])
+# D_S = np.array([2742756.14])
+# C_T3 = np.array([50.29])
+# D_T3 = np.array([50824570.13])
+# E = np.array([62.08])
+# F = np.array([946350.00])
+# CONST1 = np.array([0.85])
+# CONST2 = np.array([0.15])
 
 
 
@@ -127,7 +128,7 @@ print((x_enc*y_enc).decrypt())
 
 
 np.set_printoptions(suppress=True,
-   formatter={'float_kind':'{:0.2f}'.format})
+   formatter={'float_kind':'{:0.3f}'.format})
 C_T1_ENC = ts.ckks_vector(ctx, C_T1)
 D_T1_ENC = ts.ckks_vector(ctx, D_T1)
 A_M1_ENC = ts.ckks_vector(ctx, A_M1)
@@ -149,22 +150,26 @@ F_ENC = ts.ckks_vector(ctx, F)
 CONST1_ENC = ts.ckks_vector(ctx, CONST1)
 CONST2_ENC = ts.ckks_vector(ctx, CONST2)
 
+
+start = time.time()
 area1 = (C_T1*D_T1) - (A_M1*B_M1) - (A_M2*(B_M2*0.15)) - (C_DT*D_DT) - (C_CR*D_CR)
 area1_enc = (C_T1_ENC*D_T1_ENC) - (A_M1_ENC*B_M1_ENC) - (A_M2_ENC*(B_M2_ENC*CONST2_ENC)) - (C_DT_ENC*D_DT_ENC) - (C_CR_ENC*D_CR_ENC)
-print(f'\nArea 1 Expected: {area1[0]}')
-print(f'Area 1 Actual:   {area1_enc.decrypt()[0]}')
+print(f'\nArea 1 Expected: {area1.tolist()}')
+print(f'Area 1 Actual:   {area1_enc.decrypt()}')
 
 
 area2 = (C_T2*D_T2) - (C_S*D_S)
 area2_enc = (C_T2_ENC*D_T2_ENC) - (C_S_ENC*D_S_ENC)
-print(f'\nArea 2 Expected: {area2[0]}')
-print(f'Area 2 Actual:   {area2_enc.decrypt()[0]}')
+print(f'\nArea 2 Expected: {area2.tolist()}')
+print(f'Area 2 Actual:   {area2_enc.decrypt()}')
 
 
 area3 = ((C_T3*D_T3) - (A_M2*(B_M2*CONST1))) - (E-F)
 area3_enc = ((C_T3_ENC*D_T3_ENC) - (A_M2_ENC*(B_M2_ENC*CONST1_ENC))) - (E_ENC-F_ENC)
-print(f'\nArea 3 Expected: {area3[0]}')
-print(f'Area 3 Actual:   {area3_enc.decrypt()[0]}')
+print(f'\nArea 3 Expected: {area3.tolist()}')
+print(f'Area 3 Actual:   {area3_enc.decrypt()}')
+
+print(f'\nComputation time: {(time.time()-start)*1000} ms')
 
 
 
